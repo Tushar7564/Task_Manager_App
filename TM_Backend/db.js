f380e365b05b9b8ad34ab123fe8db40d138f2e25
Tushar7564 <tusharsinghal2703@gmail.com>
2025-05-07 20:32:22 +0530

    Add Render-compatible DB connection and package.json updation

--- TM_Backend/db.js
+++ TM_Backend/db.js
@@ -3,13 +3,27 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const pool = new Pool({
+let pool;
+
+if(process.env.DATABASE_URL) {
+    // Use DATABASE_URL (for Render)
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        },
+    });
+} else {
+    // Fallback to local dev config
+    pool = new Pool({
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_NAME || 'taskmanager',
     password: process.env.DB_PASSWORD || '',
     port: process.env.DB_PORT || 5432,
 })
+    
+}
 
 pool.connect()
     .then(() => console.log('Connected to the database'))
